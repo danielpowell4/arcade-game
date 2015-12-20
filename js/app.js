418d2ef3dc3f697d11b90502440dc163aaa10a84
danielpowell4 <danielpowell12@msn.com>
2015-12-20 12:13:29 -0700

    Add x y and speed to function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 };
 
 // Draw the enemy on the screen, required method for game
