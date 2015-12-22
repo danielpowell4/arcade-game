1cba0f3aab7a7fcd930309ef8ec3d7fb58b457fa
danielpowell4 <danielpowell12@msn.com>
2015-12-21 20:06:21 -0700

    Created an empty array called allEnemies

--- js/app.js
+++ js/app.js
@@ -63,16 +63,17 @@ player.update.handleInput = function(key) {
       var downPos = this.y + lenY; // move down
       break;
     default:
+      console.log("that's a weird button to press");
 
   }
 
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
