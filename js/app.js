c7748b6382036614a8fd759c8a369e0375fb833a
danielpowell4 <danielpowell12@msn.com>
2016-01-07 20:36:05 -0700

    found the top

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ Enemy.prototype.render = function() {
 
 Enemy.prototype.reset = function () {
   this.x = EnemyStartPositionX;
-  this.yPositions = [43];
+  this.yPositions = [63];
   this.y = this.yPositions[Math.floor(Math.random() * this.yPositions.length)];
   this.speed = Math.floor((Math.random() * 10) + 20);
 };
