224c386e6096874544963cebdfd05ff2b40ab7c6
danielpowell4 <danielpowell12@msn.com>
2016-01-07 21:32:05 -0700

    making things pretty

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ Enemy.prototype.update = function(dt) {
     this.x = this.x + (this.speed * dt);
 
     if (this.x > 530) {
-      this.reset();
-    };
+        this.reset();
+    }
 
 };
 
@@ -22,13 +22,14 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Enemy.prototype.reset = function () {
-  this.x = -101;
-  this.yPositions = [61, 144, 226, 312];
-  this.y = this.yPositions[Math.floor(Math.random() * this.yPositions.length)];
-  this.speed = Math.floor((Math.random() * 132) + 132);
+Enemy.prototype.reset = function() {
+    this.x = -101;
+    this.yPositions = [61, 144, 226, 312];
+    this.y = this.yPositions[Math.floor(Math.random() * this.yPositions.length)];
+    this.speed = Math.floor((Math.random() * 132) + 132);
 };
 
+// Our story's protagonist
 var Player = function(startX, startY) {
     this.sprite = 'images/char-princess-girl.png';
     this.height = 76;
@@ -38,79 +39,79 @@ var Player = function(startX, startY) {
     this.y = this.posStartY;
 };
 
-Player.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.update = function(keyCode) {
-  var stepX = 101;
-  var stepY = 83;
+    var stepX = 101;
+    var stepY = 83;
 
-  switch (this.action) {
-    case 'up':
-      if (this.y > 0) {
-        this.y -= stepY;
-      }
-      break;
+    switch (this.action) {
+        case 'up':
+            if (this.y > 0) {
+                this.y -= stepY;
+            }
+            break;
 
-    case 'right':
-      if (this.x < 400) {
-        this.x += stepX;
-      }
-      break;
+        case 'right':
+            if (this.x < 400) {
+                this.x += stepX;
+            }
+            break;
 
-    case 'down':
-      if (this.y < 390) {
-        this.y += stepY;
-      }
-      break;
+        case 'down':
+            if (this.y < 390) {
+                this.y += stepY;
+            }
+            break;
 
-    case 'left':
-      if (this.x > 0) {
-        this.x -= stepX;
-      }
-      break;
-  }
-
-  // reset action
-  this.action = null;
-
-  // reset if on water
-  if (this.y < 50) {
-    this.reset();
-  }
-
-  // detect collisions
-
-  for (var enemy in allEnemies) {
-    if (player.x < allEnemies[enemy].x + allEnemies[enemy].width &&
-        player.x + player.width > allEnemies[enemy].x &&
-        player.y < allEnemies[enemy].y + allEnemies[enemy].height &&
-        player.height + player.y > allEnemies[enemy].y
-     )  {
-    this.reset();
+        case 'left':
+            if (this.x > 0) {
+                this.x -= stepX;
+            }
+            break;
     }
-  }
+
+    // reset action
+    this.action = null;
+
+    // reset if on water
+    if (this.y < 50) {
+        this.reset();
+    }
+
+    // detect collisions
+
+    for (var enemy in allEnemies) {
+        if (player.x < allEnemies[enemy].x + allEnemies[enemy].width &&
+            player.x + player.width > allEnemies[enemy].x &&
+            player.y < allEnemies[enemy].y + allEnemies[enemy].height &&
+            player.height + player.y > allEnemies[enemy].y
+        ) {
+            this.reset();
+        }
+    }
 };
 
 Player.prototype.handleInput = function(e) {
-  this.action = e;
+    this.action = e;
 };
 
 Player.prototype.reset = function() {
-  this.x = 202;
-  this.y = this.posStartY;
+    this.x = 202;
+    this.y = this.posStartY;
 };
 
 // Release the enemies!!!
 var allEnemies = [];
 
 for (i = 0; i < 6; i++) {
-  var startY = Math.floor(Math.random() * 250) + 61;
-  allEnemies.push(new Enemy(-101, startY));
+    var startY = Math.floor(Math.random() * 250) + 61;
+    allEnemies.push(new Enemy(-101, startY));
 }
 
-// and the player
+// Have the Playter begin their quest
 var player = new Player();
 
 
