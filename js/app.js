9551251fb53e8576d8326780066c01cf73193272
danielpowell4 <danielpowell12@msn.com>
2016-01-07 21:01:00 -0700

    speeding bugs up

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ Enemy.prototype.reset = function () {
   this.x = EnemyStartPositionX;
   this.yPositions = [61, 144, 226, 312];
   this.y = this.yPositions[Math.floor(Math.random() * this.yPositions.length)];
-  this.speed = Math.floor((Math.random() * 10) + 20);
+  this.speed = Math.floor((Math.random() * 70) + 100);
 };
 
 var Player = function(startX, startY) {
